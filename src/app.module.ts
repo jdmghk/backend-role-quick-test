367f0d077e31001f001fa20e2c92a9db68a65add
jdmghk <jacinthdavid70@gmail.com>
2025-12-16 12:45:09 +0100

    feat: implement initial wallet and transaction management modules.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WalletsModule } from './wallets/wallets.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [],
+  imports: [WalletsModule, TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
